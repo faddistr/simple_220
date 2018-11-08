d31806237d8658ec73461b99002761c25f13db9d
Dmytro Fedorchenko <dmytro.fedorchenko@lemberg.co.uk>
2018-11-08 23:53:11 +0200

    Moved API to multiinstance architecture

--- main/telegram.h
+++ main/telegram.h
@@ -5,7 +5,9 @@
 #define TELEGRAM_MAX_TOKEN_LEN 	128U
 #define TELEGRAM_SERVER 		"https://api.telegram.org"
 
+typedef void( *telegram_on_msg_cb_t)(void *teleCtx, double chat_id, const char *from, const char *text);
 
-void telegram_init(const char *token);
-void telegram_stop(void);
+void telegram_send_text_message(void *teleCtx_ptr, double chat_id, const char *message);
+void *telegram_init(const char *token, telegram_on_msg_cb_t cb);
+void telegram_stop(void *teleCtx);
 #endif // TELEGRAM_H

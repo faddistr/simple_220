74746e82f9d8250b119d82eeb95e430398d02093
faddistr <faddistr@ukr.net>
2019-08-19 14:30:00 +0300

    Added timezone setting. Changed default sntp server.

--- main/sntp.c
+++ main/sntp.c
@@ -10,9 +10,10 @@
 #include "module.h"
 #include "cmd_executor.h"
 
-#define DEFAULT_SNTP_SERVER "pool.ntp.org"
+#define DEFAULT_SNTP_SERVER "1.ua.pool.ntp.org"
+#define DEFAULT_TIME_ZONE 	"CET-2CEST,M3.5.0/2,M10.5.0/3"
 
-static const char *TAG="SNTP";
+static const char *TAG="SNTP_MODULE";
 
 static void init_sntp_client(void);
 static void sntp_notification_cb(struct timeval *tv);
@@ -24,56 +25,79 @@ static void ip_event_handler(void *ctx, esp_event_base_t event_base, int32_t eve
 
 static void sntp_notification_cb(struct timeval *tv)
 {
+	struct timeval tv_now;
 	char strftime_buf[64];
     time_t now;
-    struct tm timeinfo;
+    struct tm *timeinfo;
+	
+	now = tv->tv_sec;
+	timeinfo = localtime(&now);
 
 	ESP_LOGI(TAG, "Time sync...");
-	settimeofday(tv, NULL);
-	localtime_r(&now, &timeinfo);
-    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+	strftime(strftime_buf, sizeof(strftime_buf), "%Y-%m-%d %H:%M:%S", timeinfo);
 
+	ESP_LOGI(TAG, "Received date/time is: %s", strftime_buf);
+
+	gettimeofday(&tv_now, NULL);
+
+	now = tv_now.tv_sec;
+	timeinfo = localtime(&now);
+
+
+	strftime(strftime_buf, sizeof(strftime_buf), "%Y-%m-%d %H:%M:%S", timeinfo);
     ESP_LOGI(TAG, "The current date/time is: %s", strftime_buf);
 }
 
 static void init_sntp_client(void)
 {
-	char *sntp_server = var_get("SNTP_SERVER");
+	char *sntp_server = var_get("SNTP_SERVER_ADDRESS");
     sntp_setoperatingmode(SNTP_OPMODE_POLL);
 
 	if (sntp_server == NULL)
 	{   
-    	sntp_setservername(0, DEFAULT_SNTP_SERVER);    } else
+    	sntp_setservername(0, DEFAULT_SNTP_SERVER);    
+    } else
     {
     	sntp_setservername(0, sntp_server);
     	free(sntp_server);	
     }
 
     sntp_set_time_sync_notification_cb(sntp_notification_cb);
-    sntp_set_sync_mode(SNTP_SYNC_MODE_SMOOTH);
+    sntp_set_sync_mode(SNTP_SYNC_MODE_IMMED);
     sntp_init();
 }
 
 static void init_sntp_module(void)
 {
+	char *tz = var_get("TIME_ZONE");
+
 	ESP_LOGI(TAG, "Initializing SNTP module...");
-	//TODO
-//	setenv("TZ", "EST5EDT,M3.2.0/2,M11.1.0", 1);
-//	tzset();
+	if (tz == NULL)
+	{
+		setenv("TZ", DEFAULT_TIME_ZONE, 1);
+	} else
+	{
+		setenv("TZ", tz, 1);
+		free(tz);
+	}
+
+	tzset();
 	ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, ip_event_handler, NULL));
 }
 
 static void cmd_gettime(const char *cmd_name, cmd_additional_info_t *info, void *private)
 {
+	struct timeval tv_now;
 	char strftime_buf[64];
     time_t now;
-    struct tm timeinfo;
-    
-    telegram_event_msg_t *evt = (telegram_event_msg_t *)info->cmd_data;
-    localtime_r(&now, &timeinfo);
-    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+    struct tm *timeinfo;
+	telegram_event_msg_t *evt = (telegram_event_msg_t *)info->cmd_data;
 
-    telegram_send_text(info->arg, evt->chat_id, NULL, "%s %d", strftime_buf, sntp_get_sync_status());
+    gettimeofday(&tv_now, NULL);
+    now = tv_now.tv_sec;
+	timeinfo = localtime(&now);
+	strftime(strftime_buf, sizeof(strftime_buf), "%Y-%m-%d %H:%M:%S", timeinfo);
+    telegram_send_text(info->arg, evt->chat_id, NULL, "%s", strftime_buf);
 }
 
 cmd_register_static({
